a0acfb6b1dc5250d39b7ea0f3b6ed5560a915497
Shawn Smith <srsmith11@live.com>
2020-05-19 14:44:59 -0600

    deleted the user--force

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,6 @@ ActiveRecord::Schema.define(version: 2020_05_19_203635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user__forces", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "age"
-    t.string "hair_color"
-    t.string "eye_color"
-    t.string "gender"
-    t.boolean "alive"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
